82ad3e5e3c92c4291ed29f2fac2e861c1021ddfc
A2zzzz <sehtel.azzedine@gmail.com>
2024-11-29 11:31:49 +0100

    Ajout des fonctionnalités formulaires

--- script.js
+++ script.js
@@ -93,18 +93,20 @@ viewButtons.forEach((btn) => {
 	})
 })
 
+const editButtons = document.querySelectorAll(".edit");
+editButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        editModal(e.target.getAttribute("data-edit-id"));
+    });
+});
 
 function editModal(gameId) {
-	// console.log(gameId, gamesList)
-	// Trouvez le jeu en fonction de son identifiant
 	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-	fetch("./form.html").then((data) => {
-		data.text().then((form) => {
+	fetch("./form.html").then((response) => {
+		response.text().then((form) => {
 			modifyModal("Mode Edition", form)
 		})
 	})
-	// passer une image comme corps du modal
-	// const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
 }
 
 function modifyModal(modalTitle, modalBody) {
